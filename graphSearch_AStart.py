c98cbdc18a4df9273ab8db66540b380ba259b475
Mohammadsaleh Razmi <mohammadsaleh.razmi@bionic-power.com>
2023-02-23 22:07:38 -0800

    Add method\function headers for functions

--- graphSearch_AStart.py
+++ graphSearch_AStart.py
@@ -4,6 +4,7 @@ import math
 import csv
 
 def read_csv_file(file_path, data):
+     """Read csv file and ignore the comment line"""
      with open(file_path) as csv_file:
           csv_reader = csv.reader(csv_file)
           for row in csv_reader:
@@ -20,10 +21,12 @@ edge_data = read_csv_file('edges.csv', edge_data)
 #print(edge_data)
 
 class searchCondiition(Enum):
+     """Enum defintion for Open and close nodes"""
      CLOSE = True
      OPEN = False
 
 class node_position:
+     """The 2D position represent the position of a node in a 2-dimnesional space."""
      def __init__(self,x, y):
       self.x = x
       self.y = y
@@ -40,12 +43,14 @@ class Graph:
         self.nodes = {}
 
     def add_node(self, node):
+        """Add a node new node to the graph with three new parameters: ID, Position, and heurusitic cost"""
         self.nodes[node.id] = node.id
         self.nodes[node.node_position] = node.node_position
         self.nodes[node.heuristic_cost] = node.heuristic_cost
 
 
     def add_edge(self, node1, node2, weight=0):
+        """Add a edge between two nodes with specified weight associated to it"""
         if node1.id not in self.nodes:
             self.add_node(node1)
         if node2.id not in self.nodes:
@@ -56,6 +61,7 @@ class Graph:
 graph = Graph()
 
 def node_extraxor(data):
+    """Extract nodes after read csv file"""
     for row in range(len(data)):
         id = int(data[row][0])
         position = node_position(float(data[row][1]), float(data[row][2]))
@@ -65,6 +71,7 @@ def node_extraxor(data):
 
 
 def edge_extractor(data):
+    """Extract edges after read csv file"""
     print(len(data))
     for row in range(len(data)):
         node_print = list(graph.nodes.values())[int(data[row][0])*3-3]
