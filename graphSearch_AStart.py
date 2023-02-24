0d3f58ab3daa33236ff4752c557e3ef14acf6b33
Mohammadsaleh Razmi <mohammadsaleh.razmi@bionic-power.com>
2023-02-23 23:56:15 -0800

    Add minimum set of parameter to initialize node
    * Limit the node node initialization to id.
    * Manage the edge with weight and neighbor.

--- graphSearch_AStart.py
+++ graphSearch_AStart.py
@@ -20,7 +20,7 @@ edge_data = read_csv_file('edges.csv', edge_data)
 
 #print(edge_data)
 
-class searchCondiition(Enum):
+class searchCondition(Enum):
      """Enum defintion for Open and close nodes"""
      CLOSE = True
      OPEN = False
@@ -32,43 +32,79 @@ class node_position:
       self.y = y
 
 class Node:
-    def __init__(self, id,node_position, heuristic_cost):
+    def __init__(self, id):
         self.id = id
-        self.node_position = node_position
-        self.heuristic_cost = heuristic_cost
         self.edges = {}
+        self.heuristic_cost = 0
+        self.position = {}
+        
+    def add_heu_cost(self, heuristic_cost):
+        self.heuristic_cost = heuristic_cost
+
+    def add_position(self, x, y):
+        self.position[0] = x
+        self.position[1] = y
+
+    def add_edge(self, neighbor, weight =0):
+        self.edges[neighbor] = weight
 
 class Graph:
     def __init__(self):
         self.nodes = {}
 
-    def add_node(self, node):
+    def add_node(self, id):
         """Add a node new node to the graph with three new parameters: ID, Position, and heurusitic cost"""
-        self.nodes[node.id] = node.id
-        self.nodes[node.node_position] = node.node_position
-        self.nodes[node.heuristic_cost] = node.heuristic_cost
-
-
-    def add_edge(self, node1, node2, weight=0):
-        """Add a edge between two nodes with specified weight associated to it"""
-        if node1.id not in self.nodes:
+        self.nodes[id] = Node(id)
+    
+    def add_position(self, node1 , x, y):
+        self.nodes[node1].add_position(x, y)
+    
+    def add_heuristic_cost(self, node1 ,heuristic_cost):
+        self.nodes[node1].add_heu_cost(heuristic_cost)
+        
+    def add_edge(self, node1, node2, weight = 0):
+        if node1 not in self.nodes:
             self.add_node(node1)
-        if node2.id not in self.nodes:
+        if node2 not in self.nodes:
             self.add_node(node2)
-        node1.add_edge(node2, weight)
-        node2.add_edge(node1, weight)
+        self.nodes[node1].add_edge(node2, weight)
+        self.nodes[node2].add_edge(node1, weight)
 
-graph = Graph()
+# Create a new Graph instance
+g = Graph()
+
+# Add nodes to the graph
+g.add_node(1)
+g.add_node(2)
+g.add_node(3)
+g.add_node(4)
+
+g.add_position(1, 1, 2)
+g.add_heuristic_cost(1, 0.2)
+
+# Add edges to the graph
+g.add_edge(1, 2, 5)
+g.add_edge(1, 3, 3)
+g.add_edge(2, 3, 1)
+g.add_edge(2, 4, 2)
+g.add_edge(3, 4, 4)
+ 
+for node_id, node in g.nodes.items():
+    print(f"Node {node_id}:")
+    print(node.heuristic_cost)
+    for neighbor, weight in node.edges.items():
+        print(f"  edge to node {neighbor} with weight {weight}")
 
 def node_extraxor(data):
     """Extract nodes after read csv file"""
     for row in range(len(data)):
         id = int(data[row][0])
+        graph.add_node(id)
         position = node_position(float(data[row][1]), float(data[row][2]))
+        graph.add_node(id)
         heuristic_cost = float(data[row][3])
         node = Node(id, position, heuristic_cost)
         graph.add_node(node)
-
 
 def edge_extractor(data):
     """Extract edges after read csv file"""
@@ -77,5 +113,3 @@ def edge_extractor(data):
         node_print = list(graph.nodes.values())[int(data[row][0])*3-3]
         print(node_print)
 
-node_extraxor(node_data)
-edge_extractor(edge_data)
